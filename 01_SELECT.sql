e1f5b0b06bfed4036f29f60e956878ee1887c750
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-09-17 11:01:21 +0900

    SELECT (JOIN)

--- 01_SELECT.sql
+++ 01_SELECT.sql
@@ -399,7 +399,50 @@ ORDER BY salary * 12 DESC;
 -- 급여 내림차순 정렬
 
 
+/***************************
+JOIN
 
+INNER JOIN : 두 테이블에서 조건을 만족하는 행만 반환
+          - 모든 고객과 그들의 최근 주문 번호
+          - 주문이 있는 상품만 주문과 주문상품 조회
+          - 로그인한 사용자의 권한 정보
+          - 결제 완료된 주문 내역
+
+LEFT JOIN  : 두 테이블에서 왼쪽 테이블의 모든 행과 오른쪽 테이블의 일치하는 행을 반환
+RIGHT JOIN : 두 테이블에서 오른쪽 테이블의 모든 행과 왼쪽 테이블의 일치하는 행을 반환
+		  - 모든 상품과 리뷰개수 (리뷰 없이도 상품 표시)
+          - 모든 고객과 그들의 최근 주문 번호 (주문이 없어도 모든 고객 표시)
+          - 직원과 교육 이수 현황(교육 안 받은 직원도 포함해서 표시)
+
+FULL OUTER JOIN : MYSQL 지원하지 않으나, UNION 예약어를 통해 구현가능
+			   - UNION : 중복 제거 후 조회
+               - UNION ALL: 중복 포함 후 조회
+               - 두 테이블의 모든 데이터를 보고싶을 때 사용
+               
+SELF JOIN : 같은 테이블의 자기 자신과 조인
+          - 계층 구조나 같은 테이블 내 관계 조회
+          - 게시글 - 답글 관계
+          - 추천인 - 피 추천인 관계
+CROSS JOIN : 두 테이블의 모든 행 조합 후 반환
+		  - 모든 조합을 만들어야 할 때
+          - 월별 실적 테이블 초기화
+          - 모든 사용자에게 기본 권한 부여
+****************************/      
+USE EMPLOYEE_MANAGEMENT;    
+
+-- JOIN 방식
+SELECT employees.dept_id, employees.full_name, departments.dept_name
+FROM employees 
+INNER JOIN departments ON employees.dept_id= departments.dept_id;
+
+SELECT E.dept_id, E.full_name, D.dept_name
+FROM employees E
+INNER JOIN departments D ON E.dept_id= D.dept_id;
+
+SELECT  E.dept_id, E.full_name, D.dept_name
+FROM employees E, departments D
+WHERE E.dept_id= D.dept_id ;
+	
 
 
 
